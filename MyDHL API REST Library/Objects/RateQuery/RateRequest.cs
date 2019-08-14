022296d917e3c8892ada56f06f43f4821d06326c
Tarek Fadel <Tarek.Fadel@dhl.com>
2019-08-14 17:52:02 +0400

    Added new Request object to RateRequest (this is where the metadata goes).

--- MyDHL API REST Library/Objects/RateQuery/RateRequest.cs
+++ MyDHL API REST Library/Objects/RateQuery/RateRequest.cs
@@ -1,5 +1,6 @@
 ﻿using MyDHLAPI_REST_Library.Objects.Common;
 using MyDHLAPI_REST_Library.Objects.Plumbing.Attributes;
+using Newtonsoft.Json;
 
 namespace MyDHLAPI_REST_Library.Objects.RateQuery
 {
@@ -7,6 +8,9 @@ namespace MyDHLAPI_REST_Library.Objects.RateQuery
     {
         [ValidateObject]
         public ClientDetails ClientDetails { get; set; } = null;
+
+        [ValidateObject]
+        public Request Request { get; set; } = new Request();
 
         [ValidateObject]
         public RequestedShipment RequestedShipment { get; set; }

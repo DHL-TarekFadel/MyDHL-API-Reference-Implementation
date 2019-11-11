b4b7988c668dff0fdbc7a92dfe5a4069ab5ce6d9
Tarek Fadel (DHL AE) <Tarek.Fadel@dhl.com>
2019-11-11 17:01:48 +0400

    Finished the JSON ad-hoc portion with syntax highlighting and code folding.

--- MyDHL API Test App/AdHoc.Designer.cs
+++ MyDHL API Test App/AdHoc.Designer.cs
@@ -44,14 +44,14 @@
             // 
             this.txtSOAP.AcceptsReturn = true;
             this.txtSOAP.AcceptsTab = true;
-            this.txtSOAP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtSOAP.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtSOAP.Enabled = false;
             this.txtSOAP.HideSelection = false;
             this.txtSOAP.Location = new System.Drawing.Point(12, 44);
             this.txtSOAP.Multiline = true;
             this.txtSOAP.Name = "txtSOAP";
             this.txtSOAP.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtSOAP.Size = new System.Drawing.Size(287, 394);
+            this.txtSOAP.Size = new System.Drawing.Size(483, 394);
             this.txtSOAP.TabIndex = 1;
             this.txtSOAP.Visible = false;
             this.txtSOAP.WordWrap = false;
@@ -59,10 +59,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Font = new System.Drawing.Font("Delivery", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(132, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "SOAP Request";
             // 
@@ -72,7 +72,7 @@
             this.btnRunJSON.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRunJSON.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.btnRunJSON.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(0)))));
-            this.btnRunJSON.Location = new System.Drawing.Point(531, 6);
+            this.btnRunJSON.Location = new System.Drawing.Point(920, 6);
             this.btnRunJSON.Name = "btnRunJSON";
             this.btnRunJSON.Size = new System.Drawing.Size(61, 35);
             this.btnRunJSON.TabIndex = 2;
@@ -83,10 +83,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(305, 28);
+            this.label2.Font = new System.Drawing.Font("Delivery", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(677, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.Size = new System.Drawing.Size(131, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "JSON Request";
             // 
@@ -94,15 +94,13 @@
             // 
             this.txtJSON.AcceptsReturn = true;
             this.txtJSON.AcceptsTab = true;
-            this.txtJSON.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtJSON.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtJSON.HideSelection = false;
-            this.txtJSON.Location = new System.Drawing.Point(305, 44);
+            this.txtJSON.Location = new System.Drawing.Point(501, 44);
             this.txtJSON.Multiline = true;
             this.txtJSON.Name = "txtJSON";
             this.txtJSON.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtJSON.Size = new System.Drawing.Size(287, 394);
+            this.txtJSON.Size = new System.Drawing.Size(483, 394);
             this.txtJSON.TabIndex = 4;
             this.txtJSON.Visible = false;
             this.txtJSON.WordWrap = false;
@@ -110,10 +108,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(598, 28);
+            this.label3.Font = new System.Drawing.Font("Delivery", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1199, 11);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(65, 24);
             this.label3.TabIndex = 5;
             this.label3.Text = "Result";
             // 
@@ -121,14 +119,14 @@
             // 
             this.txtResult.AcceptsReturn = true;
             this.txtResult.AcceptsTab = true;
-            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.txtResult.HideSelection = false;
-            this.txtResult.Location = new System.Drawing.Point(598, 44);
+            this.txtResult.Location = new System.Drawing.Point(990, 44);
             this.txtResult.Multiline = true;
             this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
             this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtResult.Size = new System.Drawing.Size(287, 394);
+            this.txtResult.Size = new System.Drawing.Size(483, 394);
             this.txtResult.TabIndex = 6;
             this.txtResult.Visible = false;
             this.txtResult.WordWrap = false;
@@ -136,10 +134,11 @@
             // btnRunSOAP
             // 
             this.btnRunSOAP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(5)))), ((int)(((byte)(17)))));
+            this.btnRunSOAP.Enabled = false;
             this.btnRunSOAP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRunSOAP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.btnRunSOAP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(0)))));
-            this.btnRunSOAP.Location = new System.Drawing.Point(238, 6);
+            this.btnRunSOAP.Location = new System.Drawing.Point(434, 6);
             this.btnRunSOAP.Name = "btnRunSOAP";
             this.btnRunSOAP.Size = new System.Drawing.Size(61, 35);
             this.btnRunSOAP.TabIndex = 7;
@@ -150,10 +149,11 @@
             // btnFromJSON
             // 
             this.btnFromJSON.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(5)))), ((int)(((byte)(17)))));
+            this.btnFromJSON.Enabled = false;
             this.btnFromJSON.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnFromJSON.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
             this.btnFromJSON.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(0)))));
-            this.btnFromJSON.Location = new System.Drawing.Point(154, 6);
+            this.btnFromJSON.Location = new System.Drawing.Point(350, 6);
             this.btnFromJSON.Name = "btnFromJSON";
             this.btnFromJSON.Size = new System.Drawing.Size(78, 35);
             this.btnFromJSON.TabIndex = 9;
@@ -164,10 +164,11 @@
             // btnFromSOAP
             // 
             this.btnFromSOAP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(5)))), ((int)(((byte)(17)))));
+            this.btnFromSOAP.Enabled = false;
             this.btnFromSOAP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnFromSOAP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
             this.btnFromSOAP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(0)))));
-            this.btnFromSOAP.Location = new System.Drawing.Point(447, 6);
+            this.btnFromSOAP.Location = new System.Drawing.Point(836, 6);
             this.btnFromSOAP.Name = "btnFromSOAP";
             this.btnFromSOAP.Size = new System.Drawing.Size(78, 35);
             this.btnFromSOAP.TabIndex = 8;
@@ -180,7 +181,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(204)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(888, 450);
+            this.ClientSize = new System.Drawing.Size(1477, 450);
             this.Controls.Add(this.btnFromJSON);
             this.Controls.Add(this.btnFromSOAP);
             this.Controls.Add(this.btnRunSOAP);
@@ -191,7 +192,11 @@
             this.Controls.Add(this.btnRunJSON);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtSOAP);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1493, 489);
+            this.MinimumSize = new System.Drawing.Size(1493, 489);
             this.Name = "AdHoc";
+            this.ShowIcon = false;
             this.Text = "Ad Hoc Request";
             this.Load += new System.EventHandler(this.AdHoc_Load);
             this.ResumeLayout(false);
--- MyDHL API Test App/AdHoc.cs
+++ MyDHL API Test App/AdHoc.cs
@@ -1,12 +1,15 @@
-﻿using ScintillaNET;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ScintillaNET;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MyDHLAPI_Test_App
@@ -16,6 +19,8 @@ namespace MyDHLAPI_Test_App
         Scintilla soapTextbox;
         Scintilla jsonTextbox;
         Scintilla resultTextbox;
+
+        private readonly HttpClient httpClient = new HttpClient();
 
         /// <summary>
         /// the background color of the text area
@@ -63,9 +68,12 @@ namespace MyDHLAPI_Test_App
             ApplyTemplate(ref txtJSON, ref jsonTextbox);
             ApplyTemplate(ref txtResult, ref resultTextbox);
 
-            InitCodeEditor(ref soapTextbox);
-            InitCodeEditor(ref jsonTextbox);
-            InitCodeEditor(ref resultTextbox);
+            InitCodeEditor(ref soapTextbox, Lexer.Xml);
+            InitCodeEditor(ref jsonTextbox, Lexer.Json);
+            InitCodeEditor(ref resultTextbox, Lexer.Null);
+
+            InitSyntaxHighlighting(ref soapTextbox);
+            InitSyntaxHighlighting(ref jsonTextbox);
 
             this.Controls.Add(soapTextbox);
             this.Controls.Add(jsonTextbox);
@@ -80,16 +88,56 @@ namespace MyDHLAPI_Test_App
             txt.Anchor = template.Anchor;
         }
 
-        private void InitCodeEditor(ref Scintilla txt)
+        private void InitCodeEditor(ref Scintilla txt, Lexer lexer)
         {
+            txt.Lexer = lexer;
+
             txt.WrapMode = WrapMode.None;
             txt.IndentationGuides = IndentView.LookBoth;
+            txt.Styles[Style.Default].Font = "Consolas";
 
+            // Setup line numbers margin
             var nums = txt.Margins[NUMBER_MARGIN];
             nums.Width = 30;
             nums.Type = MarginType.Number;
             nums.Sensitive = true;
             nums.Mask = 0;
+
+            // Setup folding
+            txt.SetProperty("fold", "1");
+            txt.SetProperty("font.compact", "1");
+
+            var folds = txt.Margins[FOLDING_MARGIN];
+            folds.Type = MarginType.Symbol;
+            folds.Mask = Marker.MaskFolders;
+            folds.Sensitive = true;
+            folds.Width = 20;
+
+            // Configure folding markers with respective symbols
+            txt.Markers[Marker.Folder].Symbol = MarkerSymbol.BoxPlus;
+            txt.Markers[Marker.FolderOpen].Symbol = MarkerSymbol.BoxMinus;
+            txt.Markers[Marker.FolderEnd].Symbol = MarkerSymbol.BoxPlusConnected;
+            txt.Markers[Marker.FolderMidTail].Symbol = MarkerSymbol.TCorner;
+            txt.Markers[Marker.FolderOpenMid].Symbol = MarkerSymbol.BoxMinusConnected;
+            txt.Markers[Marker.FolderSub].Symbol = MarkerSymbol.VLine;
+            txt.Markers[Marker.FolderTail].Symbol = MarkerSymbol.LCorner;
+
+            // Enable automatic folding
+            txt.AutomaticFold = (AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change);
+
+        }
+
+        private void InitSyntaxHighlighting(ref Scintilla tbx)
+        {
+            switch (tbx.Lexer)
+            {
+                case Lexer.Json:
+                    tbx.Styles[Style.Json.String].ForeColor = Color.DarkBlue;
+                    tbx.Styles[Style.Json.PropertyName].ForeColor = Color.Green;
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void BtnFromJSON_Click(object sender, EventArgs e)
@@ -109,7 +157,78 @@ namespace MyDHLAPI_Test_App
 
         private void BtnRunJSON_Click(object sender, EventArgs e)
         {
+            try
+            {
+                this.UseWaitCursor = true;
+                this.Cursor = Cursors.WaitCursor;
+                this.Enabled = false;
 
+                JToken jsonObject = GetJTokenFromString(jsonTextbox.Text);
+
+                var methodProperty = ((JObject)jsonObject).Properties().First().Name;
+
+                string jsonText = string.Empty;
+
+                using (StringWriter sw = new StringWriter())
+                {
+                    using (JsonWriter jw = new JsonTextWriter(sw))
+                    {
+                        JsonSerializer.CreateDefault().Serialize(jw, jsonObject);
+
+                        jsonText = sw.ToString();
+                    }
+                }
+
+                try
+                {
+                    byte[] auth = Encoding.ASCII.GetBytes($"{Common.CurrentCredentials["Username"]}:{Common.CurrentCredentials["Password"]}");
+
+                    httpClient.DefaultRequestHeaders.Authorization
+                        = new AuthenticationHeaderValue("Basic"
+                                                        , Convert.ToBase64String(auth));
+                    HttpResponseMessage resp = httpClient.PostAsync($"{Common.CurrentRestBaseUrl}/{methodProperty}", new StringContent(jsonText, Encoding.UTF8, "application/json")).Result;
+
+                    if (!resp.IsSuccessStatusCode)
+                    {
+                        resultTextbox.Text = $"HTTP Status Code: {(int)resp.StatusCode} {resp.ReasonPhrase}";
+                    }
+                    else
+                    {
+                        JToken respJson = GetJTokenFromString(resp.Content.ReadAsStringAsync().Result);
+
+                        InitCodeEditor(ref resultTextbox, Lexer.Json);
+                        InitSyntaxHighlighting(ref resultTextbox);
+                        resultTextbox.ReadOnly = false;
+                        resultTextbox.Text = respJson.ToString();
+                        resultTextbox.ReadOnly = true;
+                    }
+                }
+                catch (WebException wex)
+                {
+                    InitSyntaxHighlighting(ref resultTextbox);
+                    resultTextbox.Text = $"Exception: {wex.Message}";
+                }
+
+            }
+            finally
+            {
+                this.Enabled = true;
+                this.UseWaitCursor = false;
+                this.Cursor = Cursors.Default;
+            }
+            return;
+        }
+
+        private JToken GetJTokenFromString(string json)
+        {
+            StringReader sr = new StringReader(json);
+            JToken jt = null;
+            using (JsonTextReader reader = new JsonTextReader(sr))
+            {
+                jt = JToken.ReadFrom(reader);
+            }
+
+            return jt;
         }
     }
 }
